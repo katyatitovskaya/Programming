f2378418408b4d556391be9567f6c57650fa33f8
katyatitovskaya <124910998+katyatitovskaya@users.noreply.github.com>
2023-09-10 17:46:36 +0700

    кастомер и итем урааа

--- src/ObjectOrientedPractics/Model/Item.cs
+++ src/ObjectOrientedPractics/Model/Item.cs
@@ -8,9 +8,51 @@ namespace ObjectOrientedPractics.Model
 {
     public class Item
     {
-        private readonly int _id;
         private string _name;
         private string _info; 
         private double _price;
+
+
+        public int Id { get; }
+
+        public static int NewId { get; set; }   
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                Services.Validator.CheckStringLength(value, 200, nameof(Name));
+                _name = value;
+            }
+        }
+        public string Info
+        {
+            get => _info;
+            set
+            {
+                Services.Validator.CheckStringLength(value, 1000, nameof(Info));
+                _info = value;
+            }
+        }
+        public double Price
+        {
+            get => _price;
+            set
+            {
+                Services.Validator.CheckPriceInRange(value, 100000, nameof(Price));
+                _price = value;
+            }
+        }
+        public Item()
+        {
+        }
+        public Item(string name, string info, double price)
+        {
+            Name = name;
+            Info = info;
+            Price = price;
+            Id = NewId;
+            NewId++;
+        }
     }
 }
