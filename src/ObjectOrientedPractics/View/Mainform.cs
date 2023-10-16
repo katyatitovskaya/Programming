f42d82626275eaf5a8ae4566ea4f447b84d7f54e
katyatitovskaya <124910998+katyatitovskaya@users.noreply.github.com>
2023-10-16 13:40:06 +0700

    2лаба вроде накоментила норм........

--- src/ObjectOrientedPractics/View/Mainform.cs
+++ src/ObjectOrientedPractics/View/Mainform.cs
@@ -5,12 +5,14 @@ namespace ObjectOrientedPractics
 {
     public partial class Mainform : Form
     {
+        /// <summary>
+        /// Объект типа <see cref="Model.Store"/>. 
+        /// </summary>
         private Store _store = new Store();
         public Mainform()
         {
             InitializeComponent();
             
-
             itemsTab.Items = _store.Items;
             customersTab.Customers =_store.Customers;
         }
