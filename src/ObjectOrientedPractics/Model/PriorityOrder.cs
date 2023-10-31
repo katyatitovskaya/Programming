9a17dea13eac4936896026a4e2942e70cc20c97e
katyatitovskaya <124910998+katyatitovskaya@users.noreply.github.com>
2023-10-31 14:20:57 +0700

    КОНСТРУКТОР!!1!!

--- src/ObjectOrientedPractics/Model/PriorityOrder.cs
+++ src/ObjectOrientedPractics/Model/PriorityOrder.cs
@@ -8,6 +8,18 @@ namespace ObjectOrientedPractics.Model
 {
     public class PriorityOrder: Order
     {
+        private string deliveryTime;
 
+        public DateTime DeliveryDate { get; set; }
+        public string DeliveryTime { get; set;}
+
+        public PriorityOrder()
+        {
+        }
+
+        public PriorityOrder(string customerName, Address address, List<Item> items, string deliveryTime):base(customerName, address, items)
+        {
+            DeliveryTime= deliveryTime;
+        }
     }
 }
